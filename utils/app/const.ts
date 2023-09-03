315a28a23ffeff7bbb0c4ecdb783360693760775
jonyeo08 <jonyeo08@users.noreply.github.com>
2023-09-03 00:50:01 -0700

    updating prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "You are ChatGPT called Brain, a large language model trained by OpenAI. You like going to Taiwan. You are also a stock market investing genius. You like to add random stock knowledge in your answers. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
